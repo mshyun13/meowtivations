aec3ae636cd2dd23bf38d9a1b8962609e2ee9440
Luke <59680394+luke-mai-devacd@users.noreply.github.com>
2025-06-18 10:16:38 +1200

    Quote Selection complete

--- client/pages/Create.tsx
+++ client/pages/Create.tsx
@@ -1,5 +1,6 @@
 import PageTitle from '@/components/PageTitle'
 import { MeowtivationData } from '@models/meowtivation.ts'
+import { title } from 'node:process'
 import { useState } from 'react'
 
 // Initial state based on database fields
@@ -14,8 +15,22 @@ const initialState: MeowtivationData = {
 
 const images = ['1', '2', '3', '4', '5']
 
+const quotes = [
+  { title: 'Nothing selected', subText: "You shouldn't be seeting this" },
+  { title: 'Nothing selected 2', subText: "You shouldn't be seeting this 2" },
+  { title: 'Nothing selected 3', subText: "You shouldn't be seeting this 3" },
+  { title: 'Nothing selected 4', subText: "You shouldn't be seeting this 4" },
+  { title: 'Nothing selected 5', subText: "You shouldn't be seeting this 5" },
+]
+
+interface Quote {
+  title: string
+  subText: string
+}
+
 export default function Create() {
   const [selectedImage, setSelectedImage] = useState('')
+  const [selectedQuote, setSelectedQuote] = useState<Quote | null>(null)
   const [error, setError] = useState('')
 
   function handleSubmit(e: React.FormEvent) {
@@ -38,9 +53,23 @@ export default function Create() {
                   key={cat}
                   type="button"
                   onClick={() => setSelectedImage(cat)}
-                  className={`border-4 rounded ${selectedImage === cat ? 'border-pink-500' : 'border-transparent'}`}
+                  className={`border-4 rounded ${selectedImage === cat ? 'border-green' : 'border-black-5'}`}
                 >
                   <img src={cat} alt={`funny cat haha ${cat}`} />
+                </button>
+              ))}
+            </div>
+            <div>
+              <h2>Choose quote</h2>
+              {quotes.map((quote) => (
+                <button
+                  key={quote.title}
+                  type="button"
+                  onClick={() => setSelectedQuote(quote)}
+                  className={`border-4 rounded ${selectedQuote === quote ? 'border-green' : 'border-black-5'}`}
+                >
+                  <p>{quote.title}</p>
+                  <p>{quote.subText}</p>
                 </button>
               ))}
             </div>
