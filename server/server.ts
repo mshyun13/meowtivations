5b9d3b916f1d208607fd4c12c1aade6ac20c64dc
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-06-17 17:29:24 +1200

    ammend server route link; debug postman post test

--- server/server.ts
+++ server/server.ts
@@ -9,7 +9,7 @@ server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
 
 server.use('/api/v1/meowtivations', meowtivationRoutes)
-server.use('/api/v1/upload', uploadRoutes)
+server.use('/api/v1/images/upload', uploadRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
