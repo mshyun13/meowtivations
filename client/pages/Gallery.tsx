7c259c90d58dcb20e78fe3b1c7e926f70ee81d5d
Luke <59680394+luke-mai-devacd@users.noreply.github.com>
2025-06-18 14:18:58 +1200

    checkout commit

--- client/pages/Gallery.tsx
+++ client/pages/Gallery.tsx
@@ -1,14 +1,24 @@
 import PageTitle from '@/components/PageTitle'
-// import Card from '../components/Card'
+import Card from '../components/Card'
+import { Meowtivation } from '@models/meowtivation'
+import { useState } from 'react'
 
 export default function Gallery() {
+  const [meowtivations, setMeowtivations] = useState<Meowtivation[]>([])
+
+
+  const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>)
   return (
     <>
       <div>
         <PageTitle title="Meowtivation Gallery" />
         <div className="text-center p-8">
           {/* Gallery Grid */}
-          <div className="grid gap-4 grid-cols-1 sm:grid=co"></div>
+          <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+            {meowtivations.map((m) => (
+              <Card key={m.userId} meowtivation={m} />
+            ))}
+          </div>
         </div>
       </div>
     </>
